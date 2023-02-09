ef316d6bc72319ef514e692c48fd04c0517dab21
Matt Couts <mattcouts7@gmail.com>
2023-02-09 14:18:11 -0600

    Moving spash screen to /loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SplashComponent } from './splash/splash.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'loading', component: SplashComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
