fb0499f07cc39e23de30a46e3ba454df6e4c17fc
Matt Couts <mattcouts7@gmail.com>
2023-04-12 12:08:26 -0500

    Don't allow game to start without players

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { GameActionComponent } from './game/game-action/game-action.component';
 import { PlayerEntryComponent } from './game/player-entry/player-entry.component';
 import { SplashComponent } from './splash/splash.component';
+import { TeamNotEmptyGuard } from './team-not-full.guard';
 
 const routes: Routes = [
   { path: 'loading', component: SplashComponent },
   { path: '', redirectTo: '/loading', pathMatch: 'full' },
   { path: 'player-entry', component: PlayerEntryComponent },
-  { path: 'game-action', component: GameActionComponent }
+  { path: 'game-action', component: GameActionComponent, canActivate: [TeamNotEmptyGuard] }
 ];
 
 @NgModule({
